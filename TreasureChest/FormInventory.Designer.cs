86859665304292611ff307298780d2d8dd467042
Wim <wim.vanherle@telenet.be>
2011-08-06 14:28:16 +0200

    Added FormInventory design

---
+++ TreasureChest/FormInventory.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace TreasureChest
+{
+	partial class FormInventory
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lstInventory = new System.Windows.Forms.ListView();
+			this.btnAdd = new System.Windows.Forms.Button();
+			this.btnClose = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// lstInventory
+			// 
+			this.lstInventory.Location = new System.Drawing.Point(12, 12);
+			this.lstInventory.Name = "lstInventory";
+			this.lstInventory.Size = new System.Drawing.Size(340, 313);
+			this.lstInventory.TabIndex = 0;
+			this.lstInventory.UseCompatibleStateImageBehavior = false;
+			// 
+			// btnAdd
+			// 
+			this.btnAdd.Location = new System.Drawing.Point(358, 12);
+			this.btnAdd.Name = "btnAdd";
+			this.btnAdd.Size = new System.Drawing.Size(75, 23);
+			this.btnAdd.TabIndex = 1;
+			this.btnAdd.Text = "Add";
+			this.btnAdd.UseVisualStyleBackColor = true;
+			// 
+			// btnClose
+			// 
+			this.btnClose.Location = new System.Drawing.Point(358, 302);
+			this.btnClose.Name = "btnClose";
+			this.btnClose.Size = new System.Drawing.Size(75, 23);
+			this.btnClose.TabIndex = 2;
+			this.btnClose.Text = "Close";
+			this.btnClose.UseVisualStyleBackColor = true;
+			// 
+			// FormInventory
+			// 
+			this.AcceptButton = this.btnClose;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(444, 337);
+			this.Controls.Add(this.btnClose);
+			this.Controls.Add(this.btnAdd);
+			this.Controls.Add(this.lstInventory);
+			this.Name = "FormInventory";
+			this.Text = "FormInventory";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListView lstInventory;
+		private System.Windows.Forms.Button btnAdd;
+		private System.Windows.Forms.Button btnClose;
+	}
+}
