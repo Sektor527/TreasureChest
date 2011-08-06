490395f954c15016d6faa320b467a54093c1deda
Wim <wim.vanherle@telenet.be>
2011-08-06 14:14:45 +0200

    Added inventory class

---
+++ TreasureChest/Inventory.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TreasureChest
+{
+	class Inventory
+	{
+		private class InventorySorter : IComparer<Item>
+		{
+			public int Compare(Item x, Item y)
+			{
+				if (x.Name.CompareTo(y.Name) != 0)
+					return x.Name.CompareTo(y.Name);
+
+				return -x.UnitPrice.CompareTo(y.UnitPrice);
+			}
+		}
+
+		private struct Item
+		{
+			internal string Name;
+			internal float UnitPrice;
+		}
+
+		private readonly List<Item> _inventory = new List<Item>();
+
+		public void Add(int count, string name, float price)
+		{
+			for (int i = 0; i < count; ++i)
+				_inventory.Add(new Item() { Name = name, UnitPrice = price });
+
+			_inventory.Sort(new InventorySorter());
+		}
+
+		public void Add(int count, string name, float price, int units)
+		{
+			for (int i = 0; i < count * units; ++i)
+				_inventory.Add(new Item() { Name = name, UnitPrice = price / units });
+
+			_inventory.Sort(new InventorySorter());
+		}
+
+		public int Count()
+		{
+			return _inventory.Count;
+		}
+
+		public int Count(string name)
+		{
+			int result = 0;
+			foreach (Item item in _inventory)
+				if (item.Name == name) result++;
+
+			return result;
+		}
+
+		public float Price(string name)
+		{
+			foreach (Item item in _inventory)
+				if (item.Name == name) return item.UnitPrice;
+
+			return 0f;
+		}
+	}
+}
---
+++ TreasureChestUnitTests/InventoryTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using NUnit.Framework;
+using TreasureChest;
+
+namespace TreasureChestUnitTests
+{
+	[TestFixture]
+	public class InventoryTests
+	{
+		[Test]
+		public void FillInventory()
+		{
+			Inventory i = new Inventory();
+
+			i.Add(1, "Lays Peper & Zout", 3.20f);
+			i.Add(1, "Hopus", 7.20f, 6);
+
+			// Counts
+			Assert.AreEqual(7, i.Count());
+			Assert.AreEqual(1, i.Count("Lays Peper & Zout"));
+			Assert.AreEqual(6, i.Count("Hopus"));
+
+			// Prices
+			Assert.IsTrue(AreEqualFloat(3.20f, i.Price("Lays Peper & Zout")));
+			Assert.IsTrue(AreEqualFloat(1.20f, i.Price("Hopus")));
+			
+			// Non-existing Counts
+			Assert.AreEqual(0, i.Count("Grimbergen kaas"));
+
+			// Non-existing Prices
+			Assert.IsTrue(AreEqualFloat(0f, i.Price("Grimbergen kaas")));
+		}
+
+		[Test]
+		public void HighestPricePerProduct()
+		{
+			Inventory i = new Inventory();
+
+			i.Add(1, "Lays Peper & Zout", 3.20f);
+			i.Add(1, "Lays Peper & Zout", 2.60f);
+			
+			Assert.IsTrue(AreEqualFloat(3.20f, i.Price("Lays Peper & Zout")));
+
+			i.Add(2, "Hopus", 3.00f);
+			i.Add(1, "Hopus", 4.00f);
+			i.Add(3, "Hopus", 5.00f);
+
+			Assert.IsTrue(AreEqualFloat(5.00f, i.Price("Hopus")));
+		}
+
+		private bool AreEqualFloat(float expected, float actual)
+		{
+			float epsilon = 0.00005f;
+			return Math.Abs(actual - expected) < epsilon;
+		}
+	}
+}
