8d2bdac8cc35a5dff96d10901de61ff8bc599fe3
vanherle <w.vanherle@softkinetic.net>
2011-08-12 07:48:26 +0200

    Created interface for Serializer class

---
+++ TreasureChest/Serializer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TreasureChest
+{
+	static class Serializer
+	{
+		internal static void Serialize(List<Session> sessions, Inventory inventory)
+		{
+			SerializeConsumers();
+			SerializeSessions(sessions);
+			SerializeInventory(inventory);
+		}
+
+		private static void SerializeConsumers()
+		{
+			throw new NotImplementedException();
+		}
+
+		private static void SerializeSessions(List<Session> sessions)
+		{
+			throw new NotImplementedException();
+		}
+
+		private static void SerializeInventory(Inventory inventory)
+		{
+			throw new NotImplementedException();
+		}
+
+		internal static void Deserialize(List<Session> sessions, Inventory inventory)
+		{
+			DeserializeConsumers();
+			DeserializeSessions(sessions);
+			DeserializeInventory(inventory);
+		}
+
+		private static void DeserializeConsumers()
+		{
+			throw new NotImplementedException();
+		}
+
+		private static void DeserializeSessions(List<Session> sessions)
+		{
+			throw new NotImplementedException();
+		}
+
+		private static void DeserializeInventory(Inventory inventory)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
