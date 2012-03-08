31fab9cccd0d0e84021fd2f3bb75f9990e56de9b
Wim <wim.vanherle@telenet.be>
2012-03-08 21:11:41 +0100

    Updated ControllerTests to better use new Controller

--- TreasureChestUnitTests/ControllerTests.cs
+++ TreasureChestUnitTests/ControllerTests.cs
@@ -11,49 +11,51 @@ namespace TreasureChestUnitTests
 	class ControllerTests
 	{
 		Controller _controller;
-		List<Session> _sessions;
-		List<Consumer> _consumers;
-		Inventory _inventory;
-
-		Consumer wim, bart, koen;
 
 		[SetUp]
 		public void SetUp()
 		{
-			_sessions = new List<Session>();
-			_consumers = new List<Consumer>();
-			_inventory = new Inventory();
-			_controller = new Controller(_sessions, _consumers, _inventory);
+			_controller = new Controller(new List<Session>(), new List<Consumer>(), new Inventory());
+		}
 
-			wim = new Consumer("wim");
-			wim.Credit = 10;
-			bart = new Consumer("bart");
-			bart.Credit = 5;
-			koen = new Consumer("koen");
-			koen.Credit = 7.5f;
+		[Test]
+		public void AddSession_Sort()
+		{
+			_controller.AddSession(new Session(new DateTime(2000, 1, 1)));
+			_controller.AddSession(new Session(new DateTime(1999, 1, 1)));
+			_controller.AddSession(new Session(new DateTime(1999, 6, 1)));
+			_controller.AddSession(new Session(new DateTime(2001, 1, 1)));
 
-			Session session = new Session();
-			session.Consumers.Add(wim);
-			session.Consumers.Add(bart);
-
-			_sessions.Add(session);
-
-			_inventory.Add(1, "cola", 30f, 6);
-			_inventory.Add(3, "chips", 3f);
+			Assert.Less(_controller.Sessions[0].Date, _controller.Sessions[1].Date);
+			Assert.Less(_controller.Sessions[1].Date, _controller.Sessions[2].Date);
+			Assert.Less(_controller.Sessions[2].Date, _controller.Sessions[3].Date);
 		}
 
 		[Test]
 		public void ConsumeItems()
 		{
-			List<Item> consumedItems = new List<Item>();
-			consumedItems.Add(_inventory.Get("chips"));
-			consumedItems.Add(_inventory.Get("cola"));
+			Consumer wim, bart, koen;
+			wim = new Consumer("wim") { Credit = 10 };
+			bart = new Consumer("bart") { Credit = 5 };
+			koen = new Consumer("koen") { Credit = 7.5f };
 
-			_controller.ConsumeItems(_sessions[0], consumedItems);
+			Session session = new Session();
+			_controller.AddSession(session);
+			_controller.AddConsumerToSession(session, wim);
+			_controller.AddConsumerToSession(session, bart);
+
+			_controller.AddItemToInventory(1, "cola", 30f, 6);
+			_controller.AddItemToInventory(3, "chips", 3f);
+
+			List<Item> consumedItems = new List<Item>();
+			consumedItems.Add(_controller.GetItemFromInventory("chips"));
+			consumedItems.Add(_controller.GetItemFromInventory("cola"));
+			
+			_controller.ConsumeItems(session, consumedItems);
 
 			// Items transferred
-			Assert.AreEqual(7, _inventory.Count());
-			Assert.AreEqual(2, _sessions[0].ConsumedItems.Count());
+			Assert.AreEqual(7, _controller.GetInventorySize());
+			Assert.AreEqual(2, session.ConsumedItems.Count());
 
 			// Credits reduced
 			Assert.AreEqual(6, wim.Credit);
@@ -66,30 +68,53 @@ namespace TreasureChestUnitTests
 		[Test]
 		public void ConsumeItems_ItemsNotInInventory()
 		{
+			Consumer wim, bart, koen;
+			wim = new Consumer("wim") { Credit = 10 };
+			bart = new Consumer("bart") { Credit = 5 };
+			koen = new Consumer("koen") { Credit = 7.5f };
+
+			Session session = new Session();
+			_controller.AddSession(session);
+			_controller.AddConsumerToSession(session, wim);
+			_controller.AddConsumerToSession(session, bart);
+
+			_controller.AddItemToInventory(1, "cola", 30f, 6);
+			_controller.AddItemToInventory(3, "chips", 3f);
+			
 			List<Item> bogusItems = new List<Item>();
 			bogusItems.Add(new Item());
 
-			_controller.ConsumeItems(_sessions[0], bogusItems);
+			_controller.ConsumeItems(session, bogusItems);
 
-			Assert.AreEqual(9, _inventory.Count());
-			Assert.AreEqual(0, _sessions[0].ConsumedItems.Count());
+			Assert.AreEqual(9, _controller.GetInventorySize());
+			Assert.AreEqual(0, session.ConsumedItems.Count());
 		}
 
 		[Test]
 		public void UnconsumeItems()
 		{
-			_sessions[0].ConsumedItems.Add(new Item {Name = "Something", UnitPrice = 1f});
-			_sessions[0].ConsumedItems.Add(new Item {Name = "Whatever", UnitPrice = 2f});
+			Consumer wim, bart, koen;
+			wim = new Consumer("wim") { Credit = 10 };
+			bart = new Consumer("bart") { Credit = 5 };
+			koen = new Consumer("koen") { Credit = 7.5f };
+
+			Session session = new Session();
+			_controller.AddSession(session);
+			_controller.AddConsumerToSession(session, wim);
+			_controller.AddConsumerToSession(session, bart);
+
+			session.ConsumedItems.Add(new Item { Name = "Something", UnitPrice = 1f });
+			session.ConsumedItems.Add(new Item {Name = "Whatever", UnitPrice = 2f});
 
 			List<Item> unconsumedItems = new List<Item>();
-			unconsumedItems.Add(_sessions[0].ConsumedItems.Get(0));
-			unconsumedItems.Add(_sessions[0].ConsumedItems.Get(1));
+			unconsumedItems.Add(session.ConsumedItems.Get(0));
+			unconsumedItems.Add(session.ConsumedItems.Get(1));
 
-			_controller.UnconsumeItems(_sessions[0], unconsumedItems);
+			_controller.UnconsumeItems(session, unconsumedItems);
 
 			// Items transferred
-			Assert.AreEqual(11, _inventory.Count());
-			Assert.AreEqual(0, _sessions[0].ConsumedItems.Count());
+			Assert.AreEqual(2, _controller.GetInventorySize());
+			Assert.AreEqual(0, session.ConsumedItems.Count());
 
 			// Credits refunded
 			Assert.AreEqual(11.5f, wim.Credit);
@@ -102,16 +127,19 @@ namespace TreasureChestUnitTests
 		[Test]
 		public void UnconsumeItems_ItemsNotInSession()
 		{
-			_sessions[0].ConsumedItems.Add(new Item { Name = "Something", UnitPrice = 1f });
-			_sessions[0].ConsumedItems.Add(new Item { Name = "Whatever", UnitPrice = 2f });
+			Session session = new Session();
+			_controller.AddSession(session);
+
+			session.ConsumedItems.Add(new Item { Name = "Something", UnitPrice = 1f });
+			session.ConsumedItems.Add(new Item { Name = "Whatever", UnitPrice = 2f });
 
 			List<Item> unconsumedItems = new List<Item>();
 			unconsumedItems.Add(new Item());
 
-			_controller.UnconsumeItems(_sessions[0], unconsumedItems);
+			_controller.UnconsumeItems(session, unconsumedItems);
 
-			Assert.AreEqual(9, _inventory.Count());
-			Assert.AreEqual(2, _sessions[0].ConsumedItems.Count());
+			Assert.AreEqual(0, _controller.GetInventorySize());
+			Assert.AreEqual(2, session.ConsumedItems.Count());
 		}
 	}
 }
