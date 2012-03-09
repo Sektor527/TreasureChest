70fdace3443f43f794409811699c22797c332898
Wim <wim.vanherle@telenet.be>
2012-03-09 11:45:59 +0100

    Created project for new interface

---
+++ TreasureChestUI/MainWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TreasureChestCore;
+
+namespace TreasureChestUI
+{
+	public partial class MainWindow : Form
+	{
+		public MainWindow(Controller controller)
+		{
+			_controller = controller;
+
+			InitializeComponent();
+		}
+
+		private Controller _controller;
+	}
+}
---
+++ TreasureChestUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using TreasureChestCore;
+
+namespace TreasureChestUI
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			Controller controller = new Controller(new List<Session>(), new List<Consumer>(), new Inventory());
+			controller.Deserialize();
+			Application.Run(new MainWindow(controller));
+			controller.Serialize();
+		}
+	}
+}
