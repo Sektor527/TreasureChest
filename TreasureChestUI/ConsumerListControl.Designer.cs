687fe0087752f02aa060c108c6f1fb3f1ba411bd
vanherle <wva@softkinetic.com>
2012-04-05 14:33:33 +0200

    Created control for list of consumers

---
+++ TreasureChestUI/ConsumerListControl.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace TreasureChestUI
+{
+	partial class ConsumerListControl
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this._manageConsumers = new System.Windows.Forms.Button();
+			this._consumerPanel = new System.Windows.Forms.FlowLayoutPanel();
+			this.SuspendLayout();
+			// 
+			// _manageConsumers
+			// 
+			this._manageConsumers.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+									| System.Windows.Forms.AnchorStyles.Right)));
+			this._manageConsumers.Location = new System.Drawing.Point(0, 253);
+			this._manageConsumers.Name = "_manageConsumers";
+			this._manageConsumers.Size = new System.Drawing.Size(196, 23);
+			this._manageConsumers.TabIndex = 5;
+			this._manageConsumers.Text = "Manage Consumers";
+			this._manageConsumers.UseVisualStyleBackColor = true;
+			this._manageConsumers.Click += new System.EventHandler(this.ManageConsumers);
+			// 
+			// _consumerPanel
+			// 
+			this._consumerPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+									| System.Windows.Forms.AnchorStyles.Left)
+									| System.Windows.Forms.AnchorStyles.Right)));
+			this._consumerPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+			this._consumerPanel.Location = new System.Drawing.Point(0, 0);
+			this._consumerPanel.Name = "_consumerPanel";
+			this._consumerPanel.Size = new System.Drawing.Size(196, 247);
+			this._consumerPanel.TabIndex = 4;
+			// 
+			// ConsumerListControl
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this._manageConsumers);
+			this.Controls.Add(this._consumerPanel);
+			this.Name = "ConsumerListControl";
+			this.Size = new System.Drawing.Size(197, 275);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button _manageConsumers;
+		private System.Windows.Forms.FlowLayoutPanel _consumerPanel;
+	}
+}
---
+++ TreasureChestUI/ConsumerListControl.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TreasureChestCore;
+
+namespace TreasureChestUI
+{
+	public partial class ConsumerListControl : UserControl
+	{
+		internal delegate void SelectionChangedDelegate(Consumer consumer, bool isChecked);
+		internal event SelectionChangedDelegate SelectionChanged;
+
+		public ConsumerListControl(Controller controller)
+		{
+			_controller = controller;
+			InitializeComponent();
+			AddRemoveConsumers();
+		}
+
+		internal void UpdateConsumers(Session session)
+		{
+			_session = session;
+
+			foreach (Control control in _consumerPanel.Controls)
+			{
+				ConsumerControl consumerControl = control as ConsumerControl;
+				if (consumerControl == null) continue;
+
+				consumerControl.Selected = session != null && _controller.IsConsumerInSession(session, consumerControl.Consumer);
+				consumerControl.UpdateCredit();
+				consumerControl.UpdateName();
+			}
+		}
+
+		internal int CheckedConsumerCount
+		{
+			get { return _consumerPanel.Controls.OfType<ConsumerControl>().Count(consumerControl => consumerControl.Selected); }
+		}
+
+		private void AddRemoveConsumers()
+		{
+			_consumerPanel.Controls.Clear();
+			foreach (Consumer c in _controller.Consumers)
+			{
+				ConsumerControl consumerControl = new ConsumerControl(_controller) { Name = c.Name, Credit = c.Credit, Tag = c };
+				_consumerPanel.Controls.Add(consumerControl);
+				consumerControl.CheckChanged += ConsumerCheckChanged;
+			}
+		}
+
+		private void ConsumerCheckChanged(object sender, EventArgs eventArgs)
+		{
+			if (SelectionChanged == null) return;
+
+			ConsumerControl control = sender as ConsumerControl;
+			if (control == null) return;
+
+			SelectionChanged(control.Consumer, control.Selected);
+		}
+
+		private void ManageConsumers(object sender, EventArgs e)
+		{
+			var window = new ManageConsumersWindow(_controller);
+			window.ShowDialog();
+			AddRemoveConsumers();
+			UpdateConsumers(_session);
+		}
+
+		private Controller _controller;
+		private Session _session;
+	}
+}
